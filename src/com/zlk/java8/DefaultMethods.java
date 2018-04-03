650f85271a2c151bce38dc72711482cad7e1ac41
zlk19921105 <2773407299@qq.com>
2018-04-03 17:50:29 +0800

    接口默认方法提交。

--- src/com/zlk/java8/DefaultMethods.java
+++ src/com/zlk/java8/DefaultMethods.java
@@ -1,8 +1,25 @@
 package com.zlk.java8;
 
-/**
+/**java8特性之接口中的默认方法
  * @author zhoulk
  *         Date:  2018/4/3.
  */
 public interface DefaultMethods {
+    double show(int a);
+
+    //特性，接口允许default修饰的实现方法
+    default double sqrt(int a){
+        return  Math.sqrt(a);
+    }
+
+    public static void main(String[] args) {
+        //立即实现
+        DefaultMethods defaultMethods = new DefaultMethods(){
+           public double show(int a){
+                return sqrt(a*100);
+            }
+        };
+        System.out.println(defaultMethods.sqrt(9)+ ";");
+        System.out.println(defaultMethods.show(100)+ ";");
+    }
 }
